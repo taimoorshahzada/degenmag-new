44f95c86a400bf965752cb9fbfe505a8214126f2
Taimoor Shahzada <m.ts131941@gmail.com>
2023-01-26 14:34:06 +0500

    changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,13 +3,15 @@
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
+import icon from "public/icon.png";
 export default function Home() {
   return (
     <div className="relative h-screen w-screen py-1 px-1 overflow-hidden">
       <div
-        className="flex relative items-center border-dotted border-[8px] border-black justify-start w-full h-full py-[10vh] flex-col text-center space-y-6 text-black"
+        className="flex relative items-center justify-start border-4 border-black border-dotted w-full h-full py-[10vh] flex-col text-center space-y-6 text-black"
         style={{ fontFamily: "consolas" }}
       >
+        <div></div>
         <h1 className="text-4xl font-bold">rn dm de gen</h1>
         <div>
           <p>We just cause scenes in web3.</p>
@@ -26,20 +28,28 @@ export default function Home() {
         </div>
         <div>Happy ヘ( ^o^)ノ＼(^_^ )</div>
         <div className="space-x-5 py-5 ">
-          <input
+          {/* <input
             type="text"
             name=""
             id=""
             className="border-2 focus:ring-0 outline-none rounded-lg py-2  px-4 text-base w-80 border-black text-black"
             placeholder="Your email here, please."
-          />
-          <button className="text-white bg-black py-2 px-8 font-bold uppercase rounded-lg">
-            Sign up
-          </button>
+          /> */}
+          <a href="mailto:labs@randomdegen.xyz">
+            <button className="text-white bg-black py-2 px-8 font-bold uppercase rounded-lg">
+              Sign up
+            </button>
+          </a>
         </div>
-        <div className="absolute bottom-0 text-xl right-0 w-52 h-52 rounded-full translate-x-20 translate-y-20  bg-[#101010] flex items-center justify-center text-white">
+        <div className="absolute bottom-0 text-xl right-0 ">
           <Link href="/items">
-            <div className="-translate-x-4 -translate-y-4">items</div>
+            {/* <div className="-translate-x-4 -translate-y-4">items</div> */}
+            <Image
+              src={icon}
+              className="w-40 object-contain"
+              alt="icon
+            "
+            />
           </Link>
         </div>
       </div>
